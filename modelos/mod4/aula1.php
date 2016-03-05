1c01d40df750bd15ac5cf0abddf3b4d601d15676
MatheusR42 <matheus.araujo.r42@gmail.com>
2016-03-05 11:24:57 -0300

    Pastas mod4 criadas

---
+++ modelos/mod4/aula1.php
@@ -0,0 +1,17 @@
+﻿<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Efeito básicos</title>
+<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="js/aula1.js"></script>
+<link rel="stylesheet" type="text/css" href="css/aula1">
+</head>
+
+<body>
+	<div class="exemplo">Aprendendo JQuery</div>
+	<span class="J_hide">Hide</span>
+	<span class="J_show">Show</span>
+	<span class="J_toggle">toggle</span>
+</body>
+</html>
