eb4447cb7fa176bbbb54b9ed50d67858d27ec7b8
Matheus Araujo <matheus.araujo.r42@gmail.com>
2016-05-27 20:13:05 -0300

    Mod7 aula5

--- modelos/mod7/js/controller.js
+++ modelos/mod7/js/controller.js
@@ -1,4 +1,56 @@
-$(function(){
+$(document).ready(function(){
 	var sender = $('form[name="enviardados"]');
 	var loader = $('.resposta');
+
+	var imageFile = $('.selectfile');
+	var inputFile =  $('input:file');
+	var campoFile = $('.filebar');
+
+
+	var bar = $('.carregando');
+	var per = $('.progress');
+
+	inputFile.css('display', 'none');
+	bar.css('display', 'none');
+
+	imageFile.click(function(){
+
+
+		inputFile.one('click', function(){
+				$(this).click();
+		}).change(function(){
+			campoFile.text($(this).val());
+		});
+	});
+
+
+	sender.submit(function(){
+		$(this).ajaxSubmit({
+			url: 'php/controller.php',
+			data: {acao: 'cadastro'},
+			beforeSubmit: function(){
+				loader.empty().html('<img src="img/load.gif" alt="carregando..." title="carregando...">Aguarde, enviando requisição</img>');
+			},
+			error: function(){
+				loader.empty().text("Desculpe, erro ao enviar requisição");
+			},
+			uploadProgress: function(event, posicao, total, completo){
+				//loader.empty().text(event + ' - '+ posicao+ ' - '+ total+' - '+completo);
+				bar.fadeIn('fast');
+				var porcentagem = completo+ '%';
+				per.width(porcentagem).text(porcentagem);
+			},
+			success: function( resposta ){
+				campoFile.empty();
+				loader.empty().html('<pre>'+resposta+'</pre>');
+
+			},
+			complete: function(){
+				//ler
+			},
+			resetForm: true
+		});
+		return false;
+	});
+
 });
--- modelos/mod7/php/controller.php
+++ modelos/mod7/php/controller.php
@@ -1,4 +1,41 @@
 <?php
 
-$up = get_meta_tags('http://www.upinside.com.br');
-print_r($up);
+//$up = get_meta_tags('http://www.upinside.com.br');
+//print_r($up);
+
+
+//print_r($_POST);
+//print_r($_FILES);
+
+require_once 'conexao.php';
+
+switch($_POST['acao']){
+
+  case 'cadastro':
+    //print_r($_POST);
+    //print_r($_FILES);
+    $titulo =  mysqli_real_escape_string( $link,  $_POST['titulo']);
+    $descricao = mysqli_real_escape_string( $link,  $_POST['descricao']);
+    $arquivo = $_FILES['arquivo'];
+    $pasta = '../uploads/';
+    if(!file_exists($pasta)) mkdir($pasta,0755);
+
+    if($arquivo['tmp_name']){
+      $extencao = pathinfo($arquivo['name'], PATHINFO_EXTENSION);
+      $filename = md5(time()).'.'.$extencao;
+
+      if(move_uploaded_file($arquivo['tmp_name'], $pasta.$filename)){
+        echo 'Arquivo enviado com sucesso!';
+      }else{
+        echo '1';
+      }
+
+    }else{
+      echo '2';
+    }
+  break;
+
+  default:
+    echo "Erro ao enviar";
+
+}
