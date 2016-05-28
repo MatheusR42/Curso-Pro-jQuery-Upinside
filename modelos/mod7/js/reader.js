9d0c4b9ae151ee530e80691b815c9c6108136fe4
Matheus Araujo <matheus.araujo.r42@gmail.com>
2016-05-28 19:14:29 -0300

    Mod7 aula7

--- modelos/mod7/js/reader.js
+++ modelos/mod7/js/reader.js
@@ -1,18 +1,50 @@
 Shadowbox.init();
 
-$(function(){
+$(function(){1
+
+  var modal = $('.modal');
+  var lista = $('.lista');
+  var menu = $('.menu li a');
+  var loader = $('.loading');
+
+  //lista.hide();
+  loader.hide();
+  modal.hide();
+
+  var local = window.location.hash;
+  local = local.replace('#','');
+
+  if(!local)
+    local = 'tudo';
+
+  menu.click(function(){
+    var local = $(this).attr('href');
+    local = local.replace('#','');
+    if(local != 'cadastro'){
+      //LER BANCO
+    }else{
+      modal.fadeIn('slow');
+      return false;
+    }
+
+  });
+
+  $('.closemodal').click(function(){
+    var local = window.location.hash;
+    local = local.replace('#','');
+    //LER BANCO
+    modal.delay(300).fadeOut('slow');
+    return false;
+  });
+
+  lista.on('click', '.actionedit', function(){
+    alert('Editar este');
+    return false;
+  });
+
+
+  lista.on('click', '.actiondelete', function(){
+    alert('Deletar este');
+    return false;
+  });
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- modelos/mod7/reader.php
+++ modelos/mod7/reader.php
@@ -25,37 +25,38 @@
 <div id="reader">
 
 	<img src="img/logohub.png" alt="Sistema de arquivos" title="Sistema de arquivos" />
-    
+
     <div class="modal">
+			<a href="#" class="closemodal">X FECHAR</a>
     	<div class="resposta">ENVIE SEU ARQUIVO:</div>
 
         <div class="carregando">
             <div class="progress">0%</div>
         </div>
-        
+
         <form name="enviardados" action="" method="post" enctype="multipart/form-data">
             <label>
                 <span class="campo">Arquivo:</span>
                 <input type="file" name="arquivo"/>
-                
+
                 <span class="filebar"></span>
                 <img class="selectfile" src="img/upload.png" />
             </label>
-        
+
             <label>
                 <span class="campo">Titulo:</span>
                 <input type="text" name="titulo" />
             </label>
-            
+
             <label>
                 <span class="campo">Descrição:</span>
                 <textarea name="descricao" role="3"></textarea>
             </label>
-            
-            <input type="submit" value="Enviar Dados" class="btn" />	
+
+            <input type="submit" value="Enviar Dados" class="btn" />
         </form>
-        
-        <a href="#" class="closemodal">X FECHAR</a>
+
+
     </div>
 
     <ul class="menu">
@@ -65,9 +66,9 @@
         <li><a class="nav" href="#imagens">Ver Imagens</a></li>
         <li><a class="nav" href="#videos">Ver Vídeos</a></li>
     </ul>
-    
+
     <div class="loading"><img src="img/load.gif" alt="Carregando..." title="Carregando..." /> Aguarde, carregando informações!</div>
-    
+
     <ul class="lista">
     <?php for($i=1;$i<=3;$i++):?>
         <li class="file">
@@ -77,11 +78,11 @@
             <p class="data">Enviado em: 22/03/2013 às 14:30h</p>
             <a href="baixar">Baixar arquivo!</a>
             <div class="manage">
-            	<a href="editar"><img src="img/edit.png" alt="" title="" /></a>
-                <a href="deletar"><img src="img/delete.png" alt="" title="" /></a>
+            	<a class="actionedit" href="editar"><img src="img/edit.png" alt="" title="" /></a>
+              <a class="actiondelete" href="deletar"><img src="img/delete.png" alt="" title="" /></a>
             </div>
         </li>
-        
+
         <li class="file">
         	<img src="tim.php?src=uploads/images/9479f1e02ce8a5bb66ef2fa71ba21846.jpg&w=273&h=120" alt="Ver imagem" title="Ver Imagem" width="273" height="120" />
             <h2>Minha imagem está aqui</h2>
@@ -89,7 +90,7 @@
             <p class="data">Enviado em: 22/03/2013 às 14:30h</p>
             <a href="ver">Ver Imagem!</a>
         </li>
-        
+
         <li class="file right">
         	<img src="img/videothumb.jpg" alt="Assistir vídeo" title="Assistir vídeo" width="273" height="120" />
             <h2>Meu Vídeo está aqui</h2>
